0f88f37862ba621e316c2344be141e51bc1787ab
戴润淇 <dairunqi@dairunqideMacBook-Pro.local>
2017-03-29 17:24:13 +0800

    implement groups#edit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,26 +1,16 @@
 class GroupsController < ApplicationController
-  def index
-    @groups = Group.all
-  end
+
+  # 略 ...
 
   def show
     @group = Group.find(params[:id])
   end
 
-  def new
-    @group = Group.new
+  def edit
+    @group = Group.find(params[:id])
   end
 
-  def create
-    @group = Group.new(group_params)
-    @group.save
+  # 略 ...
 
-    redirect_to groups_path
-  end
 
-  private
-
- def group_params
-   params.require(:group).permit(:title, :description)
- end
 end
