4f1c37bbd839c3e5384319be0ff846f64bc6425e
戴润淇 <dairunqi@dairunqideMacBook-Pro.local>
2017-04-06 19:15:59 +0800

    implement post create function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,22 +1,17 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
-
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
   def index
     @groups = Group.all
   end
 
   def show
-    @group = Group .find(params[:id])
+    @group = Group.find(params[:id])
     @posts = @group.posts
   end
 
   def edit
-    @group = Group.find(params[:id])
-
-    if current_user != @group.user_id
-      redirect_to root_path, alert: "You have no permission."
   end
-end
 
   def new
     @group = Group.new
@@ -34,12 +29,6 @@ end
   end
 
   def update
-    @group = Group.find(params[:id])
-
-    if current_user != @group.user
-      redirect_to root_path, alert: "You have no permission."
-    end
-
     if @group.update(group_params)
       redirect_to groups_path, notice: "Update Success"
     else
@@ -48,13 +37,8 @@ end
   end
 
   def destroy
-    @group = Group.find(params[:id])
-
-    if current_user != @group.user
-      redirect_to root_path, alert: "You have no permission."
-  end
-  @group.destroy
-  redirect_to group_path, alert: "Group deleted"
+    @group.destroy
+    redirect_to group_path, alert: "Group deleted"
 end
 
   private
@@ -70,4 +54,5 @@ end
   def group_params
     params.require(:group).permit(:title, :description)
   end
+
 end
